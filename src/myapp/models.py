1f380c235813fc3c71e3dfd5bbb744d9f53f35c5
GrassEater2 <tiantirta@gmail.com>
2025-08-06 13:57:31 +0700

    Simple update in manage.py

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=False, default='Mulyono')
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
